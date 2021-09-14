96e66b2edc7608a744f12ad29c0f16be30686a96
IcyLilith <69317946+IcyLilith@users.noreply.github.com>
2021-09-14 22:14:02 +0200

    1st update

---
+++ PRG522_SA_7285/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRG522_SA_7285.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Services
+        public ActionResult Services()
+        {
+            return View();
+        }
+        // GET: Portfolio
+        [Authorize]
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        // GET: Error
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
